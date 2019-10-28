6b3edd574ce77e92c4c29e099c84be965ed8bcb8
Dominik P <git@d-pelz.de>
2019-10-28 21:23:00 +0100

    Default Settings angepasst - Neutrale IP ist nun voreingestellt - Standardmäßig wird beim wählen und abheben auf den Lautsprecher gewechselt

--- Snom-Remote-Control/Properties/Settings.Designer.cs
+++ Snom-Remote-Control/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace Snom_Remote_Control.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("10.24.51.220")]
+        [global::System.Configuration.DefaultSettingValueAttribute("0.0.0.0")]
         public string PhoneIP {
             get {
                 return ((string)(this["PhoneIP"]));
@@ -169,7 +169,7 @@ namespace Snom_Remote_Control.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2")]
         public byte AutoSwitchOutput {
             get {
                 return ((byte)(this["AutoSwitchOutput"]));
